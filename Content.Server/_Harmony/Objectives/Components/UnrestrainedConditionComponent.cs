599db2f3d4ddd0ff3d2f74b164cd44c9aa6a1294
youtissoum <51883137+youtissoum@users.noreply.github.com>
2024-12-21 18:45:02 +0100

    add perma prisoner objectives

---
+++ Content.Server/_Harmony/Objectives/Components/UnrestrainedConditionComponent.cs
@@ -0,0 +1,9 @@
+﻿using Content.Server._Harmony.Objectives.Systems;
+
+namespace Content.Server._Harmony.Objectives.Components;
+
+/// <summary>
+/// Requires that the player is unrestrained.
+/// </summary>
+[RegisterComponent, Access(typeof(UnrestrainedConditionSystem))]
+public sealed partial class UnrestrainedConditionComponent : Component;
