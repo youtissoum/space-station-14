1ba4d6518fda2a02943938e8e648fefdca74f27e
youtissoum <51883137+youtissoum@users.noreply.github.com>
2024-12-21 17:53:03 +0100

    add perma prisoner gamerule and antag selection

---
+++ Content.Server/_Harmony/Roles/PermaPrisonerRoleComponent.cs
@@ -0,0 +1,9 @@
+﻿using Content.Shared.Roles;
+
+namespace Content.Server._Harmony.Roles;
+
+/// <summary>
+///     Added to mind role entities to tag that they are a perma prisoner.
+/// </summary>
+[RegisterComponent]
+public sealed partial class PermaPrisonerRoleComponent : BaseMindRoleComponent;
