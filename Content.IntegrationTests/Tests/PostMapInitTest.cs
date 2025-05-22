dfde7921a3d239f03cda2650743f94862cddb03c
Spanky <scott@wearejacob.com>
2025-05-22 16:11:25 -0400

    Clean up PostMapInitTest.cs (#653)

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -70,11 +70,11 @@ namespace Content.IntegrationTests.Tests
             "dm02-sandbomb", // Harmony, deathmatch PROMOD map by Unisol
             "Xeno", // Harmony, playtest for upstream by SlamBamActionman
             "Barratry", // Harmony, revived by Spanky
-            "Aspid", // Harmony, playtest for upstream by Golinth
+            "Aspid", // Harmony, revived by Golinth and gruesomegray
             "Atlas", // Harmony revived by Kravin
             "Mira", // Harmony, developed by tanuko
-            "Eclipse", //Harmony, Developed by Lachryphage
-			"Prime", //Harmony, Developed/revived by luckyshotpictures
+            "Eclipse", // Harmony, developed by Lachryphage
+            "Prime", // Harmony, developed/revived by luckyshotpictures
             "Dev",
             "TestTeg",
             "Fland",
